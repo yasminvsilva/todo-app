b39be7d05b3add8b18d239d56fbb79a82603af64
Yasmin <alunoyasminsilva07@gmail.com>
2023-11-15 20:00:54 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Olá, mundo!")
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
