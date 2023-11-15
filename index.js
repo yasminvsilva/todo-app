08f4e73b41bd86b66052bb885d9a91ed1d1faf23
Yasmin <alunoyasminsilva07@gmail.com>
2023-11-15 20:39:52 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Olá, mundo!")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
