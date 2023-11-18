3b328a16e2d1447236af5385496a8fc881a9320f
Yasmin <alunoyasminsilva07@gmail.com>
2023-11-18 20:22:30 -0300

    salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -9,6 +9,32 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+// converter dados do formulário em objeto javascript
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+// rotas
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
