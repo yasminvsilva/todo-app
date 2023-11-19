9443a9a43cf3d04f25e6a5451aad332e66098099
Yasmin <alunoyasminsilva07@gmail.com>
2023-11-18 21:08:40 -0300

    completar tarefa no banco de dados

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 // rotas
+
+app.post('/completar', (requisicao, resposta) => {
+    const id = requisicao.body.id
+    
+    const sql = `
+        UPDATE tarefas 
+        SET completa = '1'
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/criar', (requisicao, resposta) => {
     const descricao = requisicao.body.descricao
     const completa = 0
