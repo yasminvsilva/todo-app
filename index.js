cef4e9f8dc67c0720e161249dfe3869d0916e14d
Yasmin <alunoyasminsilva07@gmail.com>
2023-11-18 21:43:29 -0300

    iniciando o filtro de tarefas ativas

--- index.js
+++ index.js
@@ -72,6 +72,10 @@ app.post('/criar', (requisicao, resposta) => {
     })
 })
 
+app.get('/ativas', (requisicao, resposta) => {
+    
+})
+
 app.get('/', (requisicao, resposta) => {
     const sql = 'SELECT * FROM tarefas'
 
