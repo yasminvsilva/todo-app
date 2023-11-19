2909f163eaddfe5c60b9466e043360c5b5c8c372
Yasmin <alunoyasminsilva07@gmail.com>
2023-11-18 21:49:33 -0300

    filtro de tarefas ativas completo

--- index.js
+++ index.js
@@ -73,7 +73,28 @@ app.post('/criar', (requisicao, resposta) => {
 })
 
 app.get('/ativas', (requisicao, resposta) => {
-    
+    const sql = `
+        SELECT * FROM tarefas
+        WHERE completa = 0
+    `
+
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dado) => {
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: false
+            }
+        })
+
+        const quantidadeTarefas = tarefas.length
+
+        resposta.render('ativas', { tarefas, quantidadeTarefas })
+    })
 })
 
 app.get('/', (requisicao, resposta) => {
